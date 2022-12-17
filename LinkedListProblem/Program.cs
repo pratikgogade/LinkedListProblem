15c203b3d52f75f166f592763983e97de1973522
unknown <Pratik@Example.com>
2022-12-17 07:21:14 +0530

    [Pratik]Add:Enqueue

--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -12,10 +12,10 @@ namespace LinkedListsProblems
         static void Main(string[] args)
         {
             LinkedList list = new LinkedList();
-            list.Addnote(10);
-            list.Addnote(20);
-            list.Addnote(30);
-            list.Display();
+            //list.Addnote(10);
+            //list.Addnote(20);
+            //list.Addnote(30);
+            //list.Display();
 
             //list.Addreverse(10);
             //list.Addreverse(20);
@@ -23,25 +23,32 @@ namespace LinkedListsProblems
             //list.Display();
 
 
-            list.DeleteLast();
-            list.Display();
+            //list.DeleteLast();
+            //list.Display();
 
             Stack_Ex stack_ = new Stack_Ex();
-            stack_.Push(10);
-            stack_.Push(20);
-            stack_.Push(30);
-            stack_.Peek();
-            stack_.Display();
+            //stack_.Push(10);
+            //stack_.Push(20);
+            //stack_.Push(30);
+            //stack_.Peek();
+            //stack_.Display();
 
-            stack_.Pop();
+            //stack_.Pop();
 
-            stack_.Display();
-            stack_.Pop();
+            //stack_.Display();
+            //stack_.Pop();
 
-            stack_.Display();
-            stack_.Pop();
+            //stack_.Display();
+            //stack_.Pop();
 
-            stack_.Display();
+            //stack_.Display();
+
+            Queue_Ex queue_Ex = new Queue_Ex();
+            queue_Ex.Enqueue(10);
+            queue_Ex.Enqueue(20);
+            queue_Ex.Enqueue(30);
+            queue_Ex.Enqueue(40);
+            queue_Ex.Display();
         }
     }
 }
